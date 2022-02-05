18f9aa2da5d2ef5b0b4a15bdf9fb4899f3c314bc
Baron Namqui <1031275+cnamqui@users.noreply.github.com>
2022-02-05 10:45:59 +0800

    make it so that the overworld is not clamped if the player is too high

--- Assets/Scripts/WorldGen/Helpers/CoordinatesHelper.cs
+++ Assets/Scripts/WorldGen/Helpers/CoordinatesHelper.cs
@@ -5,35 +5,35 @@ using Unity.Mathematics;
 using UnityEngine;
 
 public static class CoordinatesHelper 
-{
-    // Start is called before the first frame update
+{ 
     public static IEnumerable<int3> CoordinatesInRadius(int3 center, int radius)
     {
 
-        var settings = ChunkManager.Instance.settings;
-        float maxOverworldHeight = settings.maxOverworldHeight;
-        int overworldStartAt = settings.overworldStartAt;
-        int topChunk = Mathf.CeilToInt(maxOverworldHeight / settings.chunkSize) + overworldStartAt;
-        int underworldDrawClamp = settings.underworldDrawClamp;
+        //var settings = ChunkManager.Instance.settings;
+        //float maxOverworldHeight = settings.maxOverworldHeight;
+        //int overworldStartAt = settings.overworldStartAt;
+        //int topChunk = Mathf.CeilToInt(maxOverworldHeight / settings.chunkSize) + overworldStartAt;
+        //int underworldDrawClamp = settings.underworldDrawClamp;
 
         for (int x= - radius; x<=radius; x++)
         {
             for(int y= -radius; y<=radius; y++)
             {
                 for(int z= - radius; z<=radius; z++)
-                {   
+                {
                     int3 currentCoord = new int3(x, y, z) + center;
-                    var distance = math.abs(math.distance(center, currentCoord));
-                    var withinUDClamp = center.y - currentCoord.y <= underworldDrawClamp;
-                    var belowHighestPoint = currentCoord.y <= topChunk;
-                    if (distance <= radius && withinUDClamp && belowHighestPoint)
+                    //var distance = math.abs(math.distance(center, currentCoord));
+                    //var withinUDClamp = center.y - currentCoord.y <= underworldDrawClamp;
+                    //var belowHighestPoint = currentCoord.y <= topChunk;
+                    //if (distance <= radius && withinUDClamp && belowHighestPoint)
+                    if (IsCoordinateInRange(center,currentCoord,radius))
                         yield return currentCoord;
                 }
             }
         }
     }
 
-    public static IEnumerable<int3> GetChunkCoordinatesDelta(int3 oldCoord, int3 newCoord, int range)
+    public static bool IsCoordinateInRange(int3 refPoint, int3 coord, float range)
     {
 
         var settings = ChunkManager.Instance.settings;
@@ -41,6 +41,25 @@ public static class CoordinatesHelper
         int overworldStartAt = settings.overworldStartAt;
         int topChunk = Mathf.CeilToInt(maxOverworldHeight / settings.chunkSize) + overworldStartAt;
         int underworldDrawClamp = settings.underworldDrawClamp;
+
+        var distance = math.abs(math.distance(refPoint, coord));
+        var withinUDClamp = refPoint.y - coord.y <= underworldDrawClamp;
+        var withinOverworldClamp = (float)overworldStartAt - coord.y <= underworldDrawClamp;//chunk within the clamp from the overworld
+        var refPointTooFarFromOverworld = refPoint.y - overworldStartAt > underworldDrawClamp;
+        var withinClampOrOverworld = withinUDClamp || (refPointTooFarFromOverworld && withinOverworldClamp);
+
+        var belowHighestPoint = coord.y <= topChunk;
+        return distance <= range && withinClampOrOverworld && belowHighestPoint;
+    }
+
+    public static IEnumerable<int3> GetChunkCoordinatesDelta(int3 oldCoord, int3 newCoord, int range)
+    {
+
+        //var settings = ChunkManager.Instance.settings;
+        //float maxOverworldHeight = settings.maxOverworldHeight;
+        //int overworldStartAt = settings.overworldStartAt;
+        //int topChunk = Mathf.CeilToInt(maxOverworldHeight / settings.chunkSize) + overworldStartAt;
+        //int underworldDrawClamp = settings.underworldDrawClamp;
         for (int x = -range; x <= range; x++)
         {
             for (int y = -range; y <= range; y++)
@@ -48,15 +67,17 @@ public static class CoordinatesHelper
                 for (int z = -range; z <= range; z++)
                 {
                     int3 currentCoord = new int3(x, y, z) + newCoord;
-                    var distanceFromOld = math.abs(math.distance(currentCoord, oldCoord));
-                    var distanceFromNew = math.abs(math.distance(currentCoord, newCoord)); 
-                    var withinUDClampOfOld = oldCoord.y - currentCoord.y <= underworldDrawClamp;
-                    var withinUDClampOfNew = newCoord.y - currentCoord.y <= underworldDrawClamp;
-                    var belowHighestPoint = currentCoord.y <= topChunk;
+                    //var distanceFromOld = math.abs(math.distance(currentCoord, oldCoord));
+                    //var distanceFromNew = math.abs(math.distance(currentCoord, newCoord)); 
+                    //var withinUDClampOfOld = oldCoord.y - currentCoord.y <= underworldDrawClamp;
+                    //var withinUDClampOfNew = newCoord.y - currentCoord.y <= underworldDrawClamp;
+                    //var belowHighestPoint = currentCoord.y <= topChunk;
 
-                    var inRangeOfNew = (distanceFromNew <= range && withinUDClampOfNew && belowHighestPoint);
-                    var inRangeOfOld = (distanceFromOld <= range && withinUDClampOfOld && belowHighestPoint);
+                    //var inRangeOfNew = (distanceFromNew <= range && withinUDClampOfNew && belowHighestPoint);
+                    //var inRangeOfOld = (distanceFromOld <= range && withinUDClampOfOld && belowHighestPoint);
 
+                    var inRangeOfNew = IsCoordinateInRange(newCoord,currentCoord,range);
+                    var inRangeOfOld = IsCoordinateInRange(oldCoord, currentCoord, range);
                     if (inRangeOfNew && !inRangeOfOld)
                     {
                         yield return currentCoord;
@@ -85,20 +106,4 @@ public static class CoordinatesHelper
         return _coord;
     }
 
-    public static Vector3Int ToVector3Int(this int3 i)
-    {
-        return new Vector3Int(i.x, i.y, i.z);
-    }
-    public static int3 ToInt3(this Vector3Int i)
-    {
-        return new int3(i.x, i.y, i.z);
-    }
-    public static Vector3 ToVector3(this float3 i)
-    {
-        return new Vector3(i.x, i.y, i.z);
-    }
-    public static float3 ToFloat3(this Vector3 i)
-    {
-        return new float3(i.x, i.y, i.z);
-    }
 }
